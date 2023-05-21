4f439e8ca8e018024ef7f56d33ffc9fa4ec623ee
Muhammad Ahmed <127122249+muhammadahmed1780@users.noreply.github.com>
2023-05-21 15:15:40 +0500

    Add files via upload

--- SourceCode.asm
+++ SourceCode.asm
@@ -1,9 +1,17 @@
 
+; ***************************************
+; ****** LIBRARY MANAGEMENT SYSTEM ******
+; ***************************************
+
+; ***************************************
+; ************ SUBMITTED BY *************
+; ***** MUHAMMAD AHMAD 	2021-CE-43 ******
+; ***** HUSSNAIN ALI 	2021-CE-60 ******
+; ***** MIAN FARAZ   	2021-CE-44 ******
+; ***** ALI ABDULLAH    2021-CE-40 ******
+; ***************************************
 INCLUDE IRVINE32.inc
 
-; ************************************************
-; **************** Data Section ******************
-; ************************************************
 .DATA
 
 ;***********************************************
@@ -25,21 +33,24 @@ msg1	 BYTE 0AH
 REG_MSG	 BYTE "	Enter Member's Name to register: ",0
 VIEW_MEMBERS_MSG BYTE 0Ah,"	Viewing Registered Members: ",0AH, 0DH, 0
 ADD_MSG			 BYTE "	Enter Book Name & Author Name to Add: ", 0dh, 0ah,
-			 "	Separated By Comma:",0
+			 "	Separated By Comma and Write semi-colon and give space before entering: ",0
 VIEW_BOOKS_MSG BYTE 0Ah, "	Viewing Books in Library: ",  0dh, 0ah, 0
 EXIT_MSG	   BYTE 0AH,
 				    "	----------------- ",0dh, 0ah,
 				    "	Exiting Program...",0dh, 0ah,
-					"	See you again  :')",0dh, 0ah,
 					"	------------------", 0
 					
 					
 ; variables to maniulate Books & Members
 
 bool		   DWORD ?
-MEMBERS_FILE   BYTE "MEMBERS.txt",0
-BOOKS_FILE     BYTE "BOOKS.txt",0
+MEMBERS_FILE   BYTE "C:\Users\faraz\Downloads\Compressed\Library-Management-System-master\Library-Management-System-master\MEMBERS.txt",0
+BOOKS_FILE     BYTE "C:\Users\faraz\Downloads\Compressed\Library-Management-System-master\Library-Management-System-master\BOOKS.txt",0
 filehandle     DWORD ?
+filehandle_B     DWORD ?
+Book_name_size   DWORD ?
+m_sizee     DWORD ?
+
 BUFFER_SIZE = 5000
 buffer_mem   BYTE buffer_size DUP (?)
 buffer_book  BYTE buffer_size DUP (?)
@@ -98,12 +109,57 @@ main PROC
 	JE VIEW_BFILE		; taking input in 2D array
 		JMP EXIT_MENU
 
+
+;--------------------------------------
+;--------------VIEW MEMBERS------------
+;--------------------------------------
+	VIEW_M:
+		INVOKE MSG_DISPLAY, ADDR VIEW_MEMBERS_MSG
+	MOV ECX, NUM_MEMBERS
+	cmp ECX, 0
+	JE START
+	MOV EBX, 0
+OUTPUT:
+	MOV ESI, OFFSET MEMBERS
+	MOV EAX, MEMBER_SIZE
+	MUL EBX
+	ADD ESI, Eax
+	MOV EDX, ESI
+	CALL WRITESTRING
+	INC EBX
+	CALL CRLF
+LOOP OUTPUT
+		JMP START
+
+; VIEW MEMBERS FROM FILE
+VIEW_MFILE:
+	INVOKE CreateFile,
+	    ADDR MEMBERS_FILE, ; ptr to filename
+	    GENERIC_READ, ; mode = Can read
+	    DO_NOT_SHARE, ; share mode
+	    NULL, ; ptr to security attributes
+	    OPEN_ALWAYS, ; open an existing file
+	    FILE_ATTRIBUTE_NORMAL, ; normal file attribute
+	    0 ; not used
+	mov filehandle, eax ; Copy handle to variable
+	
+	invoke ReadFile,
+	    filehandle, ; file handle
+	    addr BUFFER_MEM, ; where to read
+	    BUFFER_SIZE, ; num bytes to read
+	    addr bytesRead, ; bytes actually read
+	    0
+	invoke CloseHandle,
+	filehandle
+	mov edx, offset BUFFER_MEM ; Write String
+	call WriteString
+	JMP START
+
 ;----------------------------------------
 ;------------REGISTER MEMBERS------------
 ;----------------------------------------
 	REG_M:
-		INVOKE MSG_DISPLAY, ADDR REG_MSG
-		;INVOKE STRING_INPUT, ADDR INPUT_STRING
+		INVOKE MSG_DISPLAY, ADDR REG_MSG          ;INVOKE STRING_INPUT, ADDR INPUT_STRING
 
 	MOV ESI, OFFSET MEMBERS
 	MOV EAX, MEMBER_SIZE
@@ -112,41 +168,109 @@ main PROC
 	MOV EDX, ESI
 	MOV ECX, MEMBER_SIZE
 	CALL READSTRING
+	mov m_sizee,eax
 	INC NUM_MEMBERS
 
-	;INVOKE CreateFile,
-	;ADDR MEMBERS_FILE,
-	;GENERIC_WRITE,
-	;DO_NOT_SHARE, 
-	;NULL, 
-	;OPEN_ALWAYS, 
-	;FILE_ATTRIBUTE_NORMAL, 
-	;0
-;
-;cmp eax, INVALID_HANDLE_VALUE
-	;je exit_1
-	;mov filehandle, eax
-;INVOKE SetFilePointer,
-	 ;filehandle,
-	;0, ; distance low
-	;0, ; distance high
-	;FILE_END
-	;mov eax,filehandle
-;
-	;mov edx, offset BUFFER_MEM
-	;mov ecx, 7
-	;call READSTRING
-	;mov eax, filehandle
-	;call WriteToFile
-;
-	;INVOKE SetFilePointer,
-	 ;filehandle,
-	;0, ; distance low
-	;0, ; distance high
-	;FILE_END
-;exit_1:
-	;invoke CloseHandle, filehandle
+	INVOKE CreateFile, ADDR MEMBERS_FILE, FILE_APPEND_DATA, 0, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
+    
+	MOV filehandle, EAX
+ 
+	INVOKE WriteFile, filehandle,ESI, m_sizee, ADDR bytesRead, NULL
 
-		JMP START
+	JMP START
 
 
+;------------------------------------
+;-------------VIEW BOOKS-------------
+;------------------------------------
+	VIEW_B:
+	
+	INVOKE MSG_DISPLAY, ADDR VIEW_BOOKS_MSG
+	MOV ECX, NUM_BOOKS
+	cmp ECX, 0
+	JE START
+	MOV EBX, 0
+OUTPUTB:
+	MOV ESI, OFFSET BOOKS
+	MOV EAX, BOOK_SIZE
+	MUL EBX
+	ADD ESI, Eax
+	MOV EDX, ESI
+	CALL WRITESTRING
+	INC EBX
+	CALL CRLF	
+LOOP OUTPUTB
+		
+JMP START
+; VIEW BOOKS FROM FILE
+VIEW_BFILE:
+	INVOKE CreateFile,
+	ADDR BOOKS_FILE, ; ptr to filename
+	GENERIC_READ, ; mode = Can read
+	DO_NOT_SHARE, ; share mode
+	NULL, ; ptr to security attributes
+	OPEN_ALWAYS, ; open an existing file
+	FILE_ATTRIBUTE_NORMAL, ; normal file attribute
+	0 ; not used
+	mov filehandle_B, eax ; Copy handle to variable
+	
+	invoke ReadFile,
+	filehandle_B, ; file handle
+	addr BUFFER_BOOK, ; where to read
+	BUFFER_SIZE, ; num bytes to read
+	addr bytesRead, ; bytes actually read
+	0
+	
+	invoke CloseHandle,
+	filehandle_B
+	mov edx, offset BUFFER_BOOK ; Write String
+	call WriteString
+	JMP START
+;----------------------------------
+;--------------ADD BOOKS-----------
+;----------------------------------	
+	ADD_B:
+	INVOKE MSG_DISPLAY, ADDR ADD_MSG
+	MOV ESI, OFFSET BOOKS
+	MOV EAX, BOOK_SIZE
+	MUL NUM_BOOKS
+	ADD ESI, EAX
+	MOV EDX, ESI
+	MOV ECX, BOOK_SIZE
+	CALL READSTRING
+	mov Book_name_size,EAX 
+	INC NUM_BOOKS
+    
+	INVOKE CreateFile, ADDR BOOKS_FILE, FILE_APPEND_DATA, 0, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
+    MOV filehandle_B, EAX
+ 
+	INVOKE WriteFile, filehandle_B, ESI, Book_name_size, ADDR bytesRead, NULL
+		
+	JMP START
+;-------------------------------------------
+;----------------EXIT MENU------------------
+;-------------------------------------------
+	EXIT_MENU:
+		INVOKE MSG_DISPLAY, ADDR EXIT_MSG
+	
+	invoke ExitProcess,0
+main endp
+
+;-------------------------------------------
+;--------FUNCTION TO DISPLAY A STRING-------
+;-------------------------------------------
+MSG_DISPLAY PROC USES EDX, VAR: ptr dword
+	MOV EDX, VAR
+	CALL WRITESTRING
+	RET
+	MSG_DISPLAY ENDP
+
+STRING_INPUT PROC USES EDX ECX, var: ptr dword
+		
+	MOV EDX, VAR
+	MOV ECX, 5000
+	CALL READSTRING
+	RET
+	STRING_INPUT ENDP
+
+end main
